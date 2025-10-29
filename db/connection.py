351b0b9c93caeaa2d0a0cf124511b8534a2fc36c
CKJ2025 <dbswnsdud48@gmail.com>
2025-10-29 10:23:01 +0900

    first commit

--- db/connection.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-from config.settings import DB_CONFIG
-
-def get_connection():
-    return pymysql.connect(
-        **DB_CONFIG,
-        cursorclass=pymysql.cursors.DictCursor,
-        autocommit=True
-    )
