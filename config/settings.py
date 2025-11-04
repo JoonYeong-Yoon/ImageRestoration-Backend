f59e6e1e27272baecd20ef00a3b2e5ae89372331
CKJ2025 <dbswnsdud48@gmail.com>
2025-11-04 15:27:11 +0900

    colorization 선택 기능

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ import os
 DB_CONFIG = {
     "user": "root",
     "password": "test",
-    "host": "192.168.0.9",
+    "host": "192.168.0.6",
     "port": 3307,
     "database": "ImageRestoration",
     "charset": "utf8mb4"
